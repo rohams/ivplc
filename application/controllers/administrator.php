4fe55b86e67731aa9f32206c2c79ba7f34f6e75b
rohams <rohamsameni@Rohams-MacBook-Air.local>
2013-07-02 14:57:36 -0700

    finishing the admin section of vehicle edit
    adding obsolete section to admin view

--- application/controllers/administrator.php
+++ application/controllers/administrator.php
@@ -31,6 +31,7 @@ class Administrator extends CI_Controller {
 			'admin' => true,
 			'ApproveVehicles' => $this->vehicles->return_vehicles('','0'),
                         'EditVehicles' => $this->vehicles->return_vehicles('', '2'),
+                        'ObsoleteVehicles' => $this->vehicles->return_vehicles('','3'),
 			'vehicles' => $this->vehicles->return_vehicles()
                         
 		);
