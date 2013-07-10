659072445eb31439d3f318e31ba334bc89a35a37
rohams <rohamsameni@Rohams-MacBook-Air.local>
2013-07-10 15:55:43 -0700

    Revert "Revert "Added the editform page""
    This reverts commit f87bad44dcf2e9d9e004dda425404bc5f336b501.

--- application/views/submissions/edit.php
+++ application/views/submissions/edit.php
@@ -4,12 +4,13 @@
 	<!-- Review User Submissions -->
 	<h2>Edit Submissions</h2>
 	<?php if(isset($UserVehicles)) : ?>	
-		<table id="admin">
+		<table id="user">
 			<thead><tr>
 				<th></th>
 				<th>Manufacturer</th>
 				<th>Model</th>
 				<th>Year</th>
+				<th>Submission Date and Time</th>
 				<td>Edit</td>
 			</tr></thead>
 	
@@ -18,7 +19,6 @@
 					<tr>
 						<td>
 							<div class="mask">
-								<a class="ext" href="<?=site_url();?>admin/measurements/<?=$vehicle['pk_vehicle_id'];?>">
 									<img class="hero" src="<?=base_url() . $vehicle['images'][0];?>"/>
 								</a>
 							</div>
@@ -26,11 +26,12 @@
 						<td><?=$vehicle['manufacturer'];?></td>
 						<td><?=$vehicle['model'];?></td>
 						<td><?=$vehicle['year'];?></td>
+						<td><?=$vehicle['submitted'];?></td>
 						<td>
-							<?=form_open('submissions/edit');?>
+							<?=form_open('submit/edit');?>
 								<?=form_hidden('pk_vehicle_id', $vehicle['pk_vehicle_id']);?>
 								<?=form_hidden('fk_contributor_id', $vehicle['contributor']['pk_contributor_id']);?>
-								<div id="approve"><?=form_submit('submit', 'Approve');?></div>
+								<div id="approve"><?=form_submit('submit', 'Edit');?></div>
 							<?=form_close();?>
 						</td>
 					</tr>
